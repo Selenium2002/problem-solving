1212962603f3e7ecdb60c27645114a6ad10eeb58
Ismail <Ismail@gmail.com>
2025-07-19 00:36:23 +0200

    I am still working but you can take a look

--- task#1/task1.js
+++ task#1/task1.js
@@ -1,9 +1,5 @@
 const arr1 = ["paramount","itv4","spirit"];
 const arr2 =  ["comedy","sport","religion"];
-let text = "";
-
-console.log(arr1.forEach(arr2));
-
 
 document.getElementById('demo').innerHTML = text;
 
--- task#4/task4.js
+++ task#4/task4.js
@@ -1,3 +1,17 @@
-const myString = "Another string with spaces.";
-const numberOfSpaces = myString.split(' ').length - 1;
-console.log(`Number of spaces: ${numberOfSpaces}`);
+function countSpaces(myString) {
+    let text = "";
+    const numberOfSpaces = myString.split(' ').length - 1;
+    console.log(`Number of spaces: ${numberOfSpaces}`);
+    document.getElementById('demo').innerHTML = text;
+    text += `Number of spaces: ${numberOfSpaces}`;
+}
+countSpaces("Another string with spaces.");
+countSpaces("Another  _ _ string   __with spaces_____");
+countSpaces("Anothe r str ing    spaces .   ");
+countSpaces("Another string with spaces.");
+
+// Regular Expressions
+
+
+
+
