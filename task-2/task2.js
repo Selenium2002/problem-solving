56be1b65873364a1d7a65517874a7c54f6283555
Ismail <Ismail@gmail.com>
2025-07-22 00:16:43 +0200

    Buffering......

--- task-2/task2.js
+++ task-2/task2.js
@@ -1,3 +1,25 @@
+// const numbers = [5,1,-1,3,10];
+
+console.log('test');
+
+// console.log(numbers);
+
+let smallest = (numbers) => {
+    for (i = 0 ; i < numbers.length; i++) {
+        console.log(numbers[i]);
+
+        if (numbers[i] > i) {
+            console.log(i);
+        }
+
+        else {
+            console.log('nothing');
+        }
+    }
+};
+
+smallest([5,1,-1,3,10]);
+
 
 
 
