d70f6065148cbf3ad4c175ae477616bc64ee9f17
Ismail <Ismail@gmail.com>
2025-07-27 21:09:47 +0200

    better than before, but still not finished

--- task-9/task9.js
+++ task-9/task9.js
@@ -1,33 +1,46 @@
 console.log('test');
 
+
+// Write a function that copies the first N characters from string S2 to string S1, and does not return anything.
+
 const s1 = "Java";
 const s2 = "Python";
 let mixed = "";
 
+let concatChars = num => {
+    for (i = num; i < s1.length; i++) {
+        if (num <= 3) {
+            mixed += s1[i];
+            console.log(s1[i]);
+        }
 
-const mixchar = (num) => {
-    for (i of s2) {
-        console.log(i);  
+        else if (num > 3) {
+            mixed += "It is out of the range";   
+        } 
     }
+    console.log(mixed);
     
+    console.log(s2 + mixed);
+    
+    console.log('test'); 
 };
 
-mixchar(5);
+concatChars(2);
 
 
 
 
 
-const copyCharacters = (n) => {
-    console.log('test');
-    console.log(n + 5);
-    s2WithSlice = s2.slice(0, n);
-    mixed += s1 + s2WithSlice;
+// const copyCharacters = (n) => {
+//     console.log('test');
+//     console.log(n + 5);
+//     s2WithSlice = s2.slice(0, n);
+//     mixed += s1 + s2WithSlice;
     
-};
+// };
 
-copyCharacters(2);
-console.log(mixed);
+// copyCharacters(2);
+// console.log(mixed);
 
 
 
