6f701229dfe26e8228f2d7c14f7d53276aeeaf60
Ismail <Ismail@gmail.com>
2025-07-27 20:23:07 +0200

    new updates in reverse array problem task-7

--- task-7/task7.js
+++ task-7/task7.js
@@ -1,11 +1,41 @@
-let arr = function (arrOfNum) {
+console.log('test');
+
+
+let reversedArray = arr => {
+    let newArr = [];
     console.log('test');
-    console.log(arrOfNum.reverse());
+    for (r = arr.length -1; r >= 0; r--) {
+        console.log(arr[r]);
+        newArr.push(arr[r]); //I think that I should use that method specially when I drag elements into the array 
+        
+        
+        
+    }
     
-    return document.getElementById('demo').innerHTML = `The array is  <br>  ${arrOfNum} 
-     <br>  <br>  The reversed array is  <br>  ${arrOfNum.reverse()}`;
+    // console.log(newArr += arr[r]);  //  it should work but it doesn't or maybe I will see it another time;
+    //output ----->   7,4,93,46,8,10 undefined
+    
+    console.log(newArr);
+
 }
 
-arr([10, 8, 46, 93, 4, 7]);
-console.log('test');
-console.log(arr([10, 8, 46, 93, 4, 7]));
+arrOfNums = [10, 8, 46, 93, 4, 7]
+
+console.log(arrOfNums.length);
+
+
+reversedArray(arrOfNums);
+
+
+
+// let arr = function (arrOfNum) {
+//     console.log('test');
+//     console.log(arrOfNum.reverse());
+    
+//     return document.getElementById('demo').innerHTML = `The array is  <br>  ${arrOfNum} 
+//      <br>  <br>  The reversed array is  <br>  ${arrOfNum.reverse()}`;
+// }
+
+// arr([10, 8, 46, 93, 4, 7]);
+// console.log('test');
+// console.log(arr([10, 8, 46, 93, 4, 7]));
