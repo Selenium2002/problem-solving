d6028b1c788ef1f87d1297794278c048b98df788
Ismail <Ismail@gmail.com>
2025-09-19 23:28:23 +0200

    You can search for the first occurence of any character in any string

--- javascript/task14.js
+++ javascript/task14.js
@@ -1,3 +1,4 @@
+
 let mainDiv = document.getElementById("main");
 let containerDiv = document.getElementById("container");
 let collectorDiv = document.querySelector("#collector");
@@ -5,8 +6,20 @@ let firstText = collectorDiv.firstElementChild;
 let secondText = collectorDiv.lastElementChild;
 let demo = document.getElementById("demo");
 let searchBtn = document.getElementById("search");
+let changeStyle = document.getElementsByClassName("btnstyle")[1];
 
+// let strChr = (charStr, char) => {
+//     for(i = 0; i < charStr.length; i++){
+    
+//         charStr.includes(char)?console.log("The index number is " + charStr.indexOf(char)):console.log(-1);
+//     }
 
+// }
 
-
-
+searchBtn.addEventListener("click", () => {
+    charStr = firstText.value;
+    char = secondText.value;
+    for(i = 0; i < charStr.length; i++){
+    charStr.includes(char)?demo.innerHTML = "The position of the first occurence is " + charStr.indexOf(char):demo.innerHtml = "-1";
+    }
+})
