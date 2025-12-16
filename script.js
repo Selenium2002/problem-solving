5553f9e0bf5fe0b3259e3212dfbc2e0b0c3b74ee
Ismail <Ismail@gmail.com>
2025-12-16 16:39:46 +0200

    made the change mode's button much more organized

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ console.log(document.body.classList);
 if (savedTheme === 'dark') {
     document.body.classList.add('darkmode');
     moodIcon.src = "assets/half-moon.png";
-    changeModeToggle.style.textAlign = "left";
+    moodIcon.style.marginLeft = "-70%";
     changeModeToggle.style.backgroundColor = "#4cfafaff";
 }
 else{
     document.body.classList.add('lightmode');
     moodIcon.src = "assets/sun.png";
-    changeModeToggle.style.textAlign = "right";
+    moodIcon.style.marginLeft = "70%";
     changeModeToggle.style.backgroundColor = "#103647ff";
 }
 
@@ -23,14 +23,16 @@ changeModeToggle.addEventListener("click", () => {
     document.body.classList.toggle("darkmode");
 
     if (document.body.classList.contains('darkmode')) {
+        document.body.classList.add('darkmode');
         localStorage.setItem('theme', 'dark');
         moodIcon.src = "assets/half-moon.png";
-        changeModeToggle.style.textAlign = "left";
+        moodIcon.style.marginLeft = "-70%";
         changeModeToggle.style.backgroundColor = "#4cfafaff";
     } else {
+        document.body.classList.add('lightmode');
         localStorage.setItem('theme', 'light');
         moodIcon.src = "assets/sun.png";
-        changeModeToggle.style.textAlign = "right";
+        moodIcon.style.marginLeft = "70%";
         changeModeToggle.style.backgroundColor = "#103647ff";
     }
 
